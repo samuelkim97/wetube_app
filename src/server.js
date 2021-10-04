9c9f05f5c7725eb327313e976d3ef177af5b5fd2
Samuel Kim <kbeom00@gmail.com>
2021-10-04 20:25:19 +0900

    added morgan to server.js.

--- src/server.js
+++ src/server.js
@@ -1,22 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
-
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not allowed</h1>");
-  }
-  console.log("You are allowed!");
-  next();
-};
+const logger = morgan("dev");
 
 const handleHome = (req, res) => {
   return res.send("I love middelwares!");
@@ -27,8 +15,6 @@ const handleProtected = (req, res) => {
 };
 
 app.use(logger);
-app.use(privateMiddleware);
-
 app.get("/", handleHome);
 app.get("/protected", handleProtected);
 
