1f72003ae5cadd5469be92c3a226ff352054dd70
Samuel Kim <kbeom00@gmail.com>
2021-10-05 21:11:18 +0900

    ended #4.3

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
+import { edit, remove } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-const handleUser = (req, res) => res.send("Edit User");
-
-userRouter.get("/edit", handleUser);
+userRouter.get("/edit", edit);
+userRouter.get("/remove", remove);
 
 export default userRouter;
