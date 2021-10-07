4798eeed0019d956f4f93784b9ce4822fbe0ceaa
Samuel Kim <kbeom00@gmail.com>
2021-10-07 17:26:13 +0900

    ended #5.5

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.send("Edit Videos");
+export const edit = (req, res) => res.render("edit");
 export const upload = (req, res) => res.send("Upload Videos");
 export const deleteVideo = (req, res) => res.send("Delete Videos");
 export const search = (req, res) => res.send("Search Video");
