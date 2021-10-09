63b6217f80c06caf6524d767b4624aef5fbf1a06
Samuel Kim <kbeom00@gmail.com>
2021-10-09 20:07:35 +0900

    ended #6.17

--- src/models/video.js
+++ src/models/video.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 const videoSchema = new mongoose.Schema({
   title: String,
   description: String,
-  createdAt: Date,
+  createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String }],
   meta: {
-    views: Number,
-    rating: Number,
+    views: { type: Number, default: 0, required: true },
+    rating: { type: Number, default: 0, required: true },
   },
 });
 
