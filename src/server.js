e0054952a1b4d0f0b96f0fe70536571163026a9b
samuelkim <kbeom00@gmail.com>
2021-10-14 22:11:29 +0900

    ended #7.1

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
@@ -12,7 +12,7 @@ app.use(logger);
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(express.urlencoded({ extended: true }));
-app.use("/", globalRouter);
+app.use("/", rootRouter);
 app.use("/video", videoRouter);
 app.use("/user", userRouter);
 
